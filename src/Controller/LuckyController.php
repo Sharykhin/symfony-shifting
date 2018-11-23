791a8f871826dad2c9cb2b6fbb8bb88f5af5efd9
Sharykhin <siarhei.sharykhin@gmail.com>
2018-11-23 22:05:13 +0300

    working on controllers

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,10 +13,15 @@ class LuckyController extends AbstractController
      * @Route("/lucky/number/{num<\d+>?}", name="any_lucky_number")
      *
      * @param int $num
+     * @param LoggerInterface $logger
      * @return Response
      */
-    public function number(int $num = null)
+    public function number(
+        LoggerInterface $logger,
+        int $num = null
+    )
     {
+        $logger->info("Hello world");
         return $this->render('lucky/number.html.twig', [
             'number' => $num ?? 0,
         ]);
